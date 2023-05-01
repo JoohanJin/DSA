e2c5b537455e0d4667e609cfed9d09f2198e86f3
JoohanJin <wngks3146@naver.com>
2023-05-01 14:16:16 +0900

    add explnation on the two_sum.cpp problem

--- data_structure/array/two_sum.cpp
+++ data_structure/array/two_sum.cpp
@@ -1,24 +1,29 @@
 /*
-Two Sum Problem from Leetcode
+*Two Sum Problem from Leetcode
 source: https://leetcode.com/problems/two-sum/
 
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+*You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-You can return the answer in any order.
+*You can return the answer in any order.
 */
+
+
 // Two pointers method
 // Time Complexity: O(n^2)
 // Space Complexity: O(1)
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
 class Solution {
 public:
+    // making a function that returns the vector having integer as elements
+    // it has the pointer of the vector, nums, and integer, target, as the  parameters
     vector<int> twoSum(vector<int>& nums, int target) {
+        // Two pointers are used for interation
+        // nested for loop to compare every combination of elements in the vector to find the target
         for (int i = 0; i < nums.size(); i++){
             for (int j = i + 1; j < nums.size(); j++){
                 if (nums[i] + nums[j] == target){
